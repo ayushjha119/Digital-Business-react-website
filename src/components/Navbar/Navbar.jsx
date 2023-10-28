227ce4ae2041b240de02f57c18ff9f91afb93601
ayushjha119 <ayush.jha.it23@heritageit.edu.in>
2023-10-28 15:02:25 +0530

    Navbar fully completed

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,16 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import "./Navbar.css";
+import { BiMenuAltRight } from "react-icons/bi";
+import { RxCross2 } from "react-icons/rx";
+
 export const Navbar = () => {
+  const [mobileMenuOpened, setMobileMenuOpened] = useState(false);
+
   return (
     <div className="n-wrapper">
+      {/* desktop version */}
       <div className="container">
         <div className="n-container">
           {/* leftside */}
@@ -20,6 +28,32 @@ export const Navbar = () => {
             </div>
             <div className="fund-button">Get Funded</div>
           </div>
+        </div>
+      </div>
+
+      {/* mobile tab version */}
+      <div className="nm-container">
+        {/* logo */}
+        <span>AYUSH-JHA</span>
+
+        {/* menu icon */}
+
+        {!mobileMenuOpened ? (
+          <BiMenuAltRight size={30} onClick={() => setMobileMenuOpened(true)} />
+        ) : (
+          <RxCross2 size={30} onClick={() => setMobileMenuOpened(false)} />
+        )}
+
+        {/* mobile-menu */}
+        <div
+          className="nm-menu"
+          style={{ transform: mobileMenuOpened && "translateX(0%)" }}
+        >
+          <span>What we do</span>
+          <span>How it works</span>
+          <span>Who we invest in</span>
+          <span>Testimonials</span>
+          <div className="m-funded-button">Get Funded</div>
         </div>
       </div>
     </div>
